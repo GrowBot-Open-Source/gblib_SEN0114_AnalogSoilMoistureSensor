cdda4a486be41ca2b5b818d628a0fd1ea59cab23
MrcoSchrnr <MarcoSchreiner@web.de>
2024-03-13 11:43:34 +0100

    added structure of library

---
+++ gb_SEN0114_AnalogSoilMoistureSensor.h
@@ -0,0 +1,12 @@
+#ifndef gb_SEN0114_AnalogSoilMoistureSensor.h
+#define gb_SEN0114_AnalogSoilMoistureSensor.h
+
+#include <Arduino.h>
+
+class gb_SEN0114_AnalogSoilMoistureSensor
+{
+private:
+public:
+};
+
+#endif
